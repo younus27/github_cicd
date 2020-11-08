d8285a5070213aeb94270d1b69bdd4c20144b779
Mohammad Younus <42749890+younus27@users.noreply.github.com>
2020-11-08 13:33:19 +0530

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return "Hello, world!"
 
+@app.route("/test")
+def index():
+    return "You are on the test route"
 
 if __name__ == "__main__":
     app.run()
